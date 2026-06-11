000d49b99abaceadd798ffcd7565532c0c8525f6
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-06-11 16:08:57 +0700

    feat: add str to pg UUID func

--- internal/shared/utils/pgconv.go
+++ internal/shared/utils/pgconv.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
@@ -12,17 +14,19 @@ func PgUUIDToStr(u pgtype.UUID) string {
 	return uuid.UUID(u.Bytes).String()
 }
 
+func StrToPgUUID(s string) (pgtype.UUID, error) {
+	parsed, err := uuid.Parse(s)
+	if err != nil {
+		return pgtype.UUID{}, fmt.Errorf("invalid uuid %q: %w", s, err)
+	}
+	return pgtype.UUID{Bytes: parsed, Valid: true}, nil
+}
+
 func PgTextToStrPtr(t pgtype.Text) *string {
 	if !t.Valid {
 		return nil
 	}
 	return &t.String
-}
-
-func StrToPgUUID(s string) (pgtype.UUID, error) {
-	var u pgtype.UUID
-	err := u.Scan(s)
-	return u, err
 }
 
 func StrPtrToPgText(s *string) pgtype.Text {
