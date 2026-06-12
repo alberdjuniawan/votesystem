c6e541a80b38ca3667d8ed8bd5404be18f28ebe3
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-06-12 07:28:05 +0700

    feat: add vote dto with cast vote req and vote res

---
+++ internal/modules/vote/dto.go
@@ -0,0 +1,15 @@
+package vote
+
+import "time"
+
+type CastVoteRequest struct {
+	OptionID string `json:"option_id" validate:"required,uuid"`
+}
+
+type VoteResponse struct {
+	ID        string    `json:"id"`
+	RoomID    string    `json:"room_id"`
+	UserID    string    `json:"user_id"`
+	OptionID  string    `json:"option_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
