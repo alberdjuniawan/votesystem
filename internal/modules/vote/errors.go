9ffedbbe5bc3dc04a0671685947877893a82495c
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-06-12 22:02:25 +0700

    feat: add err already vote option and max votes reached vars

--- internal/modules/vote/errors.go
+++ internal/modules/vote/errors.go
@@ -3,10 +3,12 @@ package vote
 import "errors"
 
 var (
-	ErrAlreadyVoted    = errors.New("you have already voted in this room")
-	ErrRoomNotFound    = errors.New("room not found")
-	ErrOptionNotFound  = errors.New("option not found")
-	ErrVotingClosed    = errors.New("voting is not open")
-	ErrOptionNotInRoom = errors.New("option does not belong to this room")
-	ErrInternal        = errors.New("internal server error")
+	ErrAlreadyVoted       = errors.New("you have already voted in this room")
+	ErrAlreadyVotedOption = errors.New("you have already voted for this option")
+	ErrMaxVotesReached    = errors.New("you have reached the maximum votes for this room")
+	ErrRoomNotFound       = errors.New("room not found")
+	ErrOptionNotFound     = errors.New("option not found")
+	ErrVotingClosed       = errors.New("voting is not open")
+	ErrOptionNotInRoom    = errors.New("option does not belong to this room")
+	ErrInternal           = errors.New("internal server error")
 )
