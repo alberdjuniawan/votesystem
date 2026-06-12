858f69b6186ce7bf6096ee29ed69e7e289466d44
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-06-12 21:41:48 +0700

    feat: add get vote by room user option, and get votes by room and user queries

--- internal/db/queries/votes.sql
+++ internal/db/queries/votes.sql
@@ -8,15 +8,21 @@ SELECT * FROM votes
 WHERE room_id = $1 AND user_id = $2
 LIMIT 1;
 
--- name: GetVoteCountsByRoom :many
-SELECT
-    option_id,
-    COUNT(*) AS vote_count
+-- name: GetVoteByRoomUserOption :one
+SELECT * FROM votes
+WHERE room_id = $1 AND user_id = $2 AND option_id = $3
+LIMIT 1;
+
+-- name: GetVoteCountByRoomAndUser :one
+SELECT COUNT(*) AS total
 FROM votes
-WHERE room_id = $1
-GROUP BY option_id;
+WHERE room_id = $1 AND user_id = $2;
 
 -- name: GetTotalVotesByRoom :one
 SELECT COUNT(*) AS total
 FROM votes
 WHERE room_id = $1;
+
+-- name: GetVotesByRoomAndUser :many
+SELECT * FROM votes
+WHERE room_id = $1 AND user_id = $2;
