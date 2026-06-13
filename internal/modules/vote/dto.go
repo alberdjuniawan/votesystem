6ef6644fec1c6d299ad017f6dfd5b680ea194242
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-06-13 20:51:48 +0700

    feat: add my vote response struct

--- internal/modules/vote/dto.go
+++ internal/modules/vote/dto.go
@@ -13,3 +13,10 @@ type VoteResponse struct {
 	OptionID  string    `json:"option_id"`
 	CreatedAt time.Time `json:"created_at"`
 }
+
+type MyVoteResponse struct {
+	RoomID   string         `json:"room_id"`
+	RoomType string         `json:"room_type"`
+	Votes    []VoteResponse `json:"votes"`
+	HasVoted bool           `json:"has_voted"`
+}
