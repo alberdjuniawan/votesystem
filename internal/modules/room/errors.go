63505bee44bf6be409a1830e069bc36f9c8b8243
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-06-13 21:20:42 +0700

    feat: add invalid max votes err var

--- internal/modules/room/errors.go
+++ internal/modules/room/errors.go
@@ -6,6 +6,7 @@ var (
 	ErrRoomNotFound       = errors.New("room not found")
 	ErrNotOwner           = errors.New("you are not the owner of this room")
 	ErrInvalidStatus      = errors.New("invalid status transition")
+	ErrInvalidMaxVotes    = errors.New("invalid max votes")
 	ErrRoomNotDraft       = errors.New("room can only be edited in draft status")
 	ErrShareCodeCollision = errors.New("failed to generate unique share code")
 	ErrInternal           = errors.New("internal server error")
